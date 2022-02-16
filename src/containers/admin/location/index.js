617db4943c581c2775b7c0f459704fdbfae98c36
Caleb <belac335@gmail.com>
2022-02-16 14:26:08 +0100

    add map to location tab

--- src/containers/admin/location/index.js
+++ src/containers/admin/location/index.js
@@ -1,11 +1,54 @@
 import React from "react";
 import DashHeader from "../../../containers/Dashboard/components/DashHeader";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { PuffLoader } from "react-spinners";
+import { LocationWrapper } from "./style/location.styled";
+import { customMapStyles } from "./style/mapstyles";
+import Geocode from "react-geocode";
+
+// map container style
+const containerStyle = {
+  width: "100%",
+  height: "100vh",
+  borderRadius: "4px",
+};
+
+// map's center. I'm setting the map center to be the
+// physical location of the company
+const center = {
+  lat: 6.5392,
+  lng: 3.3842,
+};
+
+const options = {
+  styles: customMapStyles,
+};
 
 const Location = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyAMGgifA5cwnmJ0cwauMcF-s1yJR34X2Jg",
+  });
+
   return (
     <React.Fragment>
       <DashHeader dashboardTitle="Location" />
-      <p>Remaining dashboard content will be added soon!</p>
+      {!isLoaded ? (
+        <div className="unique-table-loader">
+          <PuffLoader color="var(--primary)" />
+        </div>
+      ) : (
+        <LocationWrapper>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            options={options}
+          >
+            <Marker position={center} />
+          </GoogleMap>
+          <Marker position={center} label={`This user is currently at ...`} />
+        </LocationWrapper>
+      )}
     </React.Fragment>
   );
 };
---
+++ src/containers/admin/location/style/location.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LocationWrapper = styled.div`
+  @media only screen and (min-width: 0px) and (max-width: 576px) {
+    margin-top: 50px;
+  }
+
+  @media only screen and (min-width: 577px) and (max-width: 992px) {
+    margin-top: 60px;
+  }
+`;
